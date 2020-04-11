844b02183f11b6500ff23e1cfec5577e74767ae6
Hamidreza <hramini72@gmail.com>
2020-04-11 11:20:09 +0430

    adding setPrototypeOf to ReactPrimer

--- lib/primer/react-primer-class.ts
+++ lib/primer/react-primer-class.ts
@@ -1,10 +1,15 @@
 import { render } from 'react-dom';
-import { IPrimer, IPrimerElement, IPrimerTarget } from 'ui-wrapper';
+import { IPrimer, IPrimerElement, IPrimerTarget, Unit } from 'ui-wrapper';
 import { TReactElement } from '../type/element-type';
+import { ReactUnit } from '../unit/react-unit-class';
 
 export class ReactPrimer implements IPrimer<TReactElement> {
   public element: TReactElement;
   public target: HTMLElement;
+
+  public constructor() {
+    Object.setPrototypeOf(Unit.prototype, ReactUnit.prototype);
+  }
 
   public setElement(param: IPrimerElement<TReactElement>): void {
     const { element } = param;
